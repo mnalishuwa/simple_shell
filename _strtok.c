ce47f9d7ea1f19964eae51e082bfc9f76cdb0440
mnalishuwa <mnalishuwa@gmail.com>
2023-05-18 09:24:42 +0000

    Fix normalize space, to trim any trailing spaces

--- _strtok.c
+++ _strtok.c
@@ -4,47 +4,42 @@
  * main - test _strtok
  */
 
-/* int main(void) */
-/* { */
-/* 	size_t len_s, len_s2, i; */
-/* 	char *s, /\* *s2, *\/ *s3, *s4, *cln_s; */
-
-/* 	s = "       Hello\t  World\t\t     "; */
-/* 	len_s = strlen(s); */
-/* 	printf("s initial = %s, len_s = %lu\n", s, len_s); */
-/* 	cln_s = normalize_wspace(s, len_s); */
-/* 	len_s2 = strlen(s); */
-/* 	printf("s result = %s, len_s = %lu\n\n", cln_s, len_s2); */
-/* 	/\* s2 = _strtok(cln_s, ' '); *\/ */
-
-/* 	s3 = "Geeks1 for Greeks2 for The Geeks3 for Geeks4"; */
-/* 	s4 = " "; */
-/* 	char **argvec = _strtok(s3, s4); */
-
-/* 	for (i = 0; argvec[i] != NULL; i++) */
-/* 		printf("argv = %s\n", argvec[i]); */
-
-/* 	return (0); */
-/* } */
-
 int main(void)
 {
-	size_t i = 0;
-	/* char s1[] = "Geeks1 for Greeks2 for The Geeks3 for Geeks4"; */
-	/* char s1[] = "parses a string into a sequence of tokens"; */
-	char s1[] = "ls -la myfolder1 myfolder2 myfolder3";
-	char s2[] = " ";
-	char **argvec = _strtok(s1, s2);
+	size_t len_s, len_s2;
+	char *s, *cln_s;
 
-	for (i = 0; argvec[i] != NULL; i++)
-		printf("argv[%lu] = %s\n",i, argvec[i]);
+	s = "       Hello\t  World\t\t     ";
+	len_s = strlen(s);
+	printf("s initial = %s, len_s = %lu\n", s, len_s);
+	cln_s = normalize_wspace(s, len_s);
+	len_s2 = strlen(s);
+	printf("s result = %s, len_s = %lu\n\n", cln_s, len_s2);
+	/* s2 = _strtok(cln_s, ' '); */
+
 	return (0);
 }
+
+/* int main(void) */
+/* { */
+/* 	size_t i = 0; */
+/* 	/\* char s1[] = "Geeks1 for Greeks2 for The Geeks3 for Geeks4"; *\/ */
+/* 	/\* char s1[] = "parses a string into a sequence of tokens"; *\/ */
+/* 	char s1[] = "ls -la myfolder1 myfolder2 myfolder3"; */
+/* 	char s2[] = " "; */
+/* 	char **argvec = _strtok(s1, s2); */
+
+/* 	for (i = 0; argvec[i] != NULL; i++) */
+/* 		printf("argv[%lu] = %s\n",i, argvec[i]); */
+/* 	return (0); */
+/* } */
 /**
  * _strtok - parses a string into a sequence of tokens
  * Description: splits a string into tokens based on a delimeter
+ * It dynamically allocates memory, therefore the caller must free
+ * the memory after use.
  *
- * @_str; string arg
+ * @_str: string arg
  * @_delim: string, delimiter
  *
  * Return: pointer to the next token
@@ -89,6 +84,7 @@ char **_strtok(char *_str, const char *_delim)
  * consecutive spaces from the string.
  *
  * @_str: string arg
+ * @len_s: string length
  *
  * Return: string
  */
@@ -96,6 +92,7 @@ char *normalize_wspace(char *_str, size_t len_s)
 {
 	char *cln_str;
 	size_t i, j;
+	int last_char = -1;
 
 	cln_str = malloc(sizeof(char) * (len_s + 1));
 
@@ -103,7 +100,7 @@ char *normalize_wspace(char *_str, size_t len_s)
 	{
 		if (_str[i] == ' ' || _str[i] == '\t' || _str[i] == '\v')
 		{
-			if (i != 0 && i != len_s - 1)
+			if (i != 0)
 			{
 				cln_str[j] = ' ';
 				i++;
@@ -117,7 +114,16 @@ char *normalize_wspace(char *_str, size_t len_s)
 
 		cln_str[j] = _str[i];
 	}
+
 	cln_str[j] = '\0';
+	i = 0;
+	while (cln_str[i] != '\0')
+	{
+		if (cln_str[i] != ' ' && cln_str[i] != '\t' && cln_str[i] != '\n')
+			last_char = i;
+		i++;
+	}
+	cln_str[last_char + 1] = '\0';
 
 	return (cln_str);
 }
